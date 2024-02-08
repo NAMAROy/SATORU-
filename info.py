4ac516e6d4b7be88e892b3783c5833cd32b696b5
MADARAAGENT <159437624+MADARAAGENT@users.noreply.github.com>
2024-02-08 17:49:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ VRFY_IMG = environ.get("VRFY_IMG", "https://telegra.ph/file/10ce98d4446e117df493
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5347496761').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001706166169').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001706166169 -1002055465325').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '0').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
@@ -45,7 +45,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jyotimaurya891824:RKJGruscPRYR3kKO@cluster0.0pj1qy8.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://madarauchiha891824:ifowPKZlyJLEab6O@cluster0.f0xz9tb.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
@@ -58,16 +58,16 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MAX_B_TN = environ.get("MAX_B_TN", "10")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/SMK_MOVIES_WORLD_GOJO')
-SPRT_CHNL = environ.get('SPRT_CHNL', 'https://t.me/SMK_MOVIES_WORLD_GOJO')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/SMK_MOVIES_WORLD_GOJO')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/SMK_MOVIES_WORLD_GOJO')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/breakballyt')
+SPRT_CHNL = environ.get('SPRT_CHNL', 'https://t.me/avinsanthosh2004')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MOVIE_UPDATES_CRAZE')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/MOVIE_UPDATES_CRAZE')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
-HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/SMK_MOVIES_WORLD_GOJO')
+HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/MOVIE_UPDATES_CRAZE')
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002011177862'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/SMK_MOVIES_WORLD_GOJO')
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002011177862 -1002039833618'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/MOVIE_UPDATES_CRAZE')
+P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
 AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
@@ -79,7 +79,7 @@ LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False")
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001706166169')).split()]
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001706166169 -1002055465325')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
